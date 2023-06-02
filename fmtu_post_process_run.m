b3d06b0f08ee3222a422bab93a71043164ddf7fb
cralvizuri <celso.alvizuri@gmail.com>
2023-06-02 15:13:33 +0200

    update template matlab fmtu script

--- fmtu_post_process_run.m
+++ fmtu_post_process_run.m
@@ -1,3 +1,3 @@
 % 2022-06-08
-%fmtu_post_process_main('20170903033001760', 'MDJ2', 1, 0)
-get_fmtu('20200518011155328', 'crust1se', 1, 0)
+%get_fmtu('20200518011155328', 'crust1se', 1, 0) % ISC HYPOCENTER
+get_fmtu('20200518011156200', 'crust1se', 1, 0) % UPP HYPOCENTER
